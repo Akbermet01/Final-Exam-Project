40b7ab7a782f1fbf5300c346d00a376f9e7debf3
Ak <73659093+Akbermet01@users.noreply.github.com>
2020-12-16 21:45:49 +0600

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,14 @@
+function myFunction(){
+var x=1024;
+var y=9999;
+
+var deg=Math.floor(Math.random() * (x-y)) + y
+
+document.getElementById('box').style.transform="rotate("+deg+"deg");
+
+var element = document.getElementById('main');
+   element.classlist.remove('animate');
+   setTimeout(function(){
+     element.classList.add('animate')
+ }, 3000);
+ }
