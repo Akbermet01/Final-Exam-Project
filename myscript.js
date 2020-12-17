4f045a435617e3d413b6af8de67e5e6cc0c60bb6
Ak <73659093+Akbermet01@users.noreply.github.com>
2020-12-17 16:34:36 +0600

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,3 +1,22 @@
+
+
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDma8Zvm9eHxaiLIp0YOLyFV_FNSZSU9Uk",
+    authDomain: "wheel-of-fortune-dadae.firebaseapp.com",
+    projectId: "wheel-of-fortune-dadae",
+    storageBucket: "wheel-of-fortune-dadae.appspot.com",
+    messagingSenderId: "543821685490",
+    appId: "1:543821685490:web:856eddc5feb5c19a35a66b",
+    measurementId: "G-8ZXGYSWPP9"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+      
+var database = firebase.database();
+
+
+
 function myfunction(){
 var x = 1024;
 var y = 9999;
