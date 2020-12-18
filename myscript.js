40e8e6cedba2dce95a5a14dd207d0c8a3e47b5d2
Ak <73659093+Akbermet01@users.noreply.github.com>
2020-12-19 02:33:26 +0600

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -19,7 +19,7 @@ var x = 1024;
 var y = 9999;
   var deg = Math.floor(Math.random() * (x - y)) + y;
     
- document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+ document.getElementById('box').style.transform = "rotate("+ deg +"deg)";
     
     var element = 
         
